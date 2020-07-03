4fe89bb39248d48d49c69e7ce4a645beca36a505
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 16:48:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,12 @@
 class Application
 
+  @@itmes = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path=="/items"
+      resp.write "#{item}"
 
 
 
