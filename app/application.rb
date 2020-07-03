d2cda760c2d0606a9e98aa1c27a74c21f88251c0
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 16:46:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,11 @@ class Application
   # def call(env)
   #   resp = Rack::Response.new
   #   req = Rack::Request.new(env)
-  # 
+  #
   #   if req.path=="/items"
-  # 
-  # 
-  #   
+  #
+  #
+  #
   # end
 
 end
